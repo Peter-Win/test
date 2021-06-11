26cbb2a505372f5233a2385e8399b5ff3c24009d
Petr_Vinichenko@epam.com <Petr_Vinichenko@epam.com>
2021-06-11 18:25:37 +0300

    Remove all

--- clean.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del *.lock *.js *.ts *.log *.json *.md .gitignore /Q
-rmdir dist src coverage .idea .git /S /Q
-rmdir node_modules /S /Q
--- src/MainFrame.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-export const MainFrame: React.FC = () => (
-    <>
-        <h1>Hello, world!</h1>
-        <p>Generated for React + TypeScript.</p>
-    </>
-);
